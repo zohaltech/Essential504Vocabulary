fe1de7fea3254d441c88c8ac7bacf0030f883d60
ali <ali.faghani@gmail.com>
2015-10-17 17:32:23 +0330

    fix some bugs

--- app/src/main/java/com/zohaltech/app/essentialwords/classes/AlarmReceiver.java
+++ app/src/main/java/com/zohaltech/app/essentialwords/classes/AlarmReceiver.java
@@ -39,7 +39,7 @@ public class AlarmReceiver extends BroadcastReceiver
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
         resultIntent.putExtra(VocabularyDetailsActivity.VOCAB_ID, reminder.getVocabularyId());
 
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
         builder.setContentIntent(resultPendingIntent);
 
         SystemSetting setting = SystemSettings.getCurrentSettings();
@@ -53,7 +53,7 @@ public class AlarmReceiver extends BroadcastReceiver
             notification.sound = Uri.parse(setting.getRingingToneUri());
         }
         //App.notificationManager.notify((int) reminder.getTime().getTime(), builder.build());
-        App.notificationManager.notify((int) reminder.getTime().getTime(), notification);
+        App.notificationManager.notify(reminder.getVocabularyId(), notification);
 
         ReminderManager.setLastReminder(reminder);
 
