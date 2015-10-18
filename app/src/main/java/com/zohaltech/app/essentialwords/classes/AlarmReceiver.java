7c127ca7d019280c09d1fb152e6c191ff81fee85
ostademast <ostademast@yahoo.com>
2015-10-18 13:38:43 +0330

    fix notification handling

--- app/src/main/java/com/zohaltech/app/essentialwords/classes/AlarmReceiver.java
+++ app/src/main/java/com/zohaltech/app/essentialwords/classes/AlarmReceiver.java
@@ -37,19 +37,18 @@ public class AlarmReceiver extends BroadcastReceiver
 
         Intent resultIntent = new Intent(context, VocabularyDetailsActivity.class);
         resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        //resultIntent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         resultIntent.putExtra(VocabularyDetailsActivity.VOCAB_ID, reminder.getVocabularyId());
 
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, 0, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(context, reminder.getVocabularyId(), resultIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         builder.setContentIntent(resultPendingIntent);
 
         SystemSetting setting = SystemSettings.getCurrentSettings();
-        if (setting.getRingingToneUri() == null)
-        {
+        if (setting.getRingingToneUri() == null) {
             builder.setDefaults(Notification.DEFAULT_VIBRATE | Notification.DEFAULT_SOUND);
         }
         Notification notification = builder.build();
-        if (setting.getRingingToneUri() != null)
-        {
+        if (setting.getRingingToneUri() != null) {
             notification.sound = Uri.parse(setting.getRingingToneUri());
         }
         //App.notificationManager.notify((int) reminder.getTime().getTime(), builder.build());
