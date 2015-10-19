2bccc5c4bbb0fc08465fe47db8d18d4618c7acef
Macs62 <mahmoud.abdolahzadeh@gmail.com>
2015-10-19 17:42:15 +0330

    Modified : ReminderManager

--- app/src/main/java/com/zohaltech/app/essentialwords/classes/ReminderManager.java
+++ app/src/main/java/com/zohaltech/app/essentialwords/classes/ReminderManager.java
@@ -172,7 +172,7 @@ public class ReminderManager
         }
 
 
-        if (!settings.getWeekdays()[today - 1] || elapsedMinutes > startTime || getTodaySentWords() > settings.getWordsPerDay())
+        if (!settings.getWeekdays()[today - 1] || elapsedMinutes > startTime || getTodaySentWords() >= settings.getWordsPerDay())
         {
             for (int j = 1; j <= 7; j++)
             {
